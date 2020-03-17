ebea3e8692a0332fc953f1daee89c5639827ba6f
John Cosper <Johngcosper@gmail.com>
2020-03-17 18:16:21 -0400

    further fleshed out geolocationfunctions and added google maps api that loads at users long/lat position

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 //api-key: AIzaSyAsn-wPrYsLFntG4WLpIGTa6bpdzODDJDQ
 
+// Geolocation functions
 var x = document.getElementById("geoLocation");
+var userPosition = {
+    lat: '',
+    lon: ''
+};
+$(document).ready(function() {
+    getLocation();
+});
+
 function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 }
 
 function showPosition(position) {
     var x = document.getElementById("geoLocation");
     x.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
+    userPosition.lat = position.coords.latitude
+    userPosition.lon = position.coords.longitude
+    console.log(userPosition.lat, userPosition.lon);
 }
 
-$(document).on('click', '#getGeolocation', function() {
-    console.log('clicked')
-    getLocation();
-})
+// Map functions
+
+$(document).on('click', '#loadMap', function() {
+    initMap();
+});
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: userPosition.lat, lng: userPosition.lon},
+    zoom: 12
+  });
+}
